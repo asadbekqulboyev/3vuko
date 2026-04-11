19ace333ad73096d5f5a0a61fcd0b01851c05c00
asadbek <asadbekqulboyev@gmail.com>
2026-04-11 14:42:38 +0500

    start product

--- assets/js/main.js
+++ assets/js/main.js
@@ -516,16 +516,4 @@ function initPage() {
 }
 
 initPage();
-const lenis = new Lenis({
-  duration: 1.2,
-  easing: (t) => 1 - Math.pow(1 - t, 3), // smooth easing
-  smoothWheel: true,
-  smoothTouch: false,
-});
 
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
